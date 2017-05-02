d11ac952ba9883b3e6f434f35c40cc598793b113
Vredinburgh <ian@101-65.dhcp.windesheim.nl>
2017-05-02 13:03:46 +0200

    basis gelegd

---
+++ src/ordersys/Main.java
@@ -0,0 +1,13 @@
+package ordersys;
+
+/**
+ * Created by ian on 02/05/2017.
+ */
+public class Main {
+
+    public static void main(String args[]) {
+        OrdersysGUI ordersysGUI = new OrdersysGUI();
+
+    }
+
+}
---
+++ src/ordersys/tsp/TestClass.java
@@ -0,0 +1,7 @@
+package ordersys.tsp;
+
+/**
+ * Created by ian on 02/05/2017.
+ */
+public class TestClass {
+}
