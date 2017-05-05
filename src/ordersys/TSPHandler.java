bdab140d51371c96e26383cc8fefe5cf20daaa01
Gjjansen <gjjansen97@gmail.com>
2017-05-05 16:24:10 +0200

    The products an its route are now drawn in the TSP section

--- src/ordersys/TSPHandler.java
+++ src/ordersys/TSPHandler.java
@@ -6,7 +6,9 @@
 package ordersys;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import ordersys.gui.StatusOrderpickSection;
+import ordersys.tsp.AntColonyOptimization;
 import ordersys.xmlReader.Product;
 
 /**
@@ -14,19 +16,39 @@ import ordersys.xmlReader.Product;
  * @author gerri
  */
 public class TSPHandler {
-    
+
     private StatusOrderpickSection tspSection;
-    private ArrayList<Product> products;
     
+    public ArrayList<Product> products;
+
     public boolean startTsp;
-    
+
     public TSPHandler(StatusOrderpickSection tspSection, ArrayList<Product> products) {
         this.tspSection = tspSection;
         this.products = products;
     }
-    
+
     public void startTsp() {
         startTsp = true;
+        
         tspSection.repaint();
+    }
+
+    public int[] returnShortestPath() {
+        //Generate an x and y array for the product locations
+        double[] x = new double[products.size()];
+        double[] y = new double[products.size()];
+
+        //Fill the x and y array with the x and y of the products
+        for (int i = 0; i < products.size(); i++) {
+            x[i] = products.get(i).getX();
+            y[i] = products.get(i).getY();
+        }
+
+        //Create the algorithm object
+        AntColonyOptimization antColony = new AntColonyOptimization(x, y);
+
+        //Returns the shortest path calculated by the Antcolony algorithm
+        return antColony.solve();
     }
 }
--- src/ordersys/gui/StatusOrderpickSection.java
+++ src/ordersys/gui/StatusOrderpickSection.java
@@ -12,6 +12,7 @@ import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 import ordersys.Controller;
 import ordersys.TSPHandler;
+import ordersys.xmlReader.Product;
 
 /**
  *
@@ -22,18 +23,27 @@ public class StatusOrderpickSection extends JPanel {
     private Controller controller;
     private TSPHandler tsp;
 
+    private int xMultiplier;
+    private int yMultiplier;
+    
+    private int[] shortestPath;
+
     public StatusOrderpickSection(Controller controller) {
         setPreferredSize(new Dimension(395, 405));
 
         setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.black), "Status ophaalrobot"));
 
         this.controller = controller;
-        
+
+        xMultiplier = 395 / 5;
+        yMultiplier = 405 / 5;
+
         setVisible(true);
     }
 
     public void setTspHandler(TSPHandler tsp) {
         this.tsp = tsp;
+        shortestPath = tsp.returnShortestPath();
     }
 
     @Override
@@ -41,17 +51,26 @@ public class StatusOrderpickSection extends JPanel {
         super.paintComponent(g);
 
         if (tsp != null && tsp.startTsp) {
-            g.setColor(Color.green);
-            g.fillRect(315, 325, 50, 50);
-            g.setColor(Color.red);
-            g.fillRect(30, 325, 50, 50);
-            g.fillRect(167, 180, 50, 50);
-
+            for (Product p : tsp.products) {
+                g.setColor(Color.black);
+                g.drawString(p.getName(), p.getX() * xMultiplier - 60, p.getY() * yMultiplier - 60);
+                g.setColor(Color.green);
+                g.fillRect(p.getX() * xMultiplier - 60, p.getY() * yMultiplier - 60, 50, 50);
+            }
+            
             g.setColor(Color.black);
-            g.drawLine(80, 325, 167, 230);
-            g.drawLine(217, 230, 315, 325);
+            
+            //Draw the lines between the products according to the shortest path
+            for (int i = 0; i < shortestPath.length; i++) {
+                if (i + 1 < shortestPath.length) {
+                    int x1 = tsp.products.get(shortestPath[i]).getX() * xMultiplier - 30;
+                    int y1 = tsp.products.get(shortestPath[i]).getY() * yMultiplier - 30;
+                    int x2 = tsp.products.get(shortestPath[i + 1]).getX() * xMultiplier - 30;
+                    int y2 = tsp.products.get(shortestPath[i + 1]).getY() * yMultiplier - 30;
 
-            g.fillOval(217, 230, 25, 25);
+                    g.drawLine(x1, y1, x2, y2);
+                }
+            }
         }
     }
 }
